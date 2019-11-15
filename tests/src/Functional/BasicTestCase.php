1abbef142658d502978c30a0e032b9fb562a1006
Vijaya Chandran Mani <vijaycs85@gmail.com>
2019-11-15 11:26:33 +0000

    Adding node test

--- tests/src/Functional/BasicTestCase.php
+++ tests/src/Functional/BasicTestCase.php
@@ -35,11 +35,21 @@ class BasicTestCase extends BrowserTestBase {
     // Make sure to complete the normal setup steps first.
     parent::setUp();
 
+
     // Set the front page to "node".
     \Drupal::configFactory()
       ->getEditable('system.site')
       ->set('page.front', '/node')
       ->save(TRUE);
+
+    // Create Basic page and Article node types.
+    $this->drupalCreateContentType([
+      'type' => 'page',
+      'name' => 'Basic page',
+      'display_submitted' => FALSE,
+    ]);
+    $web_user = $this->drupalCreateUser(['edit own page content', 'create page content', 'administer menu']);
+    $this->drupalLogin($web_user);
   }
 
   /**
@@ -54,6 +64,25 @@ class BasicTestCase extends BrowserTestBase {
 
     // Confirm that the front page contains the standard text.
     $this->assertText('Welcome to Drupal');
+  }
+
+  /**
+   * Tests create/view of a page node.
+   */
+  public function testNode() {
+    // Add field values.
+    $edit = [
+      'title[0][value]' => $this->randomMachineName(8),
+      'body[0][value]' => $this->randomMachineName(16),
+    ];
+    // Submit the page form to create a new page.
+    $this->drupalPostForm('node/add/page', $edit, 'Save');
+    $this->assertSession()->statusCodeEquals(200);
+
+    $this->drupalGet('node/1');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains($edit['title[0][value]']);
+    $this->assertSession()->pageTextContains($edit['body[0][value]']);
   }
 
 }
